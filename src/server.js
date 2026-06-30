cf737d21c6c6b09a6561eadb91ada6581230ff25
GlowCleanconcept <contact.glowcleanconcept@gmail.com>
2026-06-30 13:35:11 +0100

    debug: log env vars

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
+console.log("JWT_SECRET défini:", !!process.env.JWT_SECRET);
+console.log("MONGO_URI défini:", !!process.env.MONGO_URI);
 
 const app = require("./app");
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 3000;
 
-// Connexion DB
 connectDB();
 
 app.listen(PORT, () => {
