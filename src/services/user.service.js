b5b7ba74e3bece1f993249c1f73a094ac9d5efe1
GlowCleanconcept <contact.glowcleanconcept@gmail.com>
2026-07-15 23:33:13 +0100

    fix: autoriser modification du champ salon

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,9 @@
 const User = require("../models/user.model");
 
 exports.getAllUsers = async (salonId, role) => {
-  // Superadmin voit tous les utilisateurs
   if (role === "superadmin") {
     return await User.find().select("-__v");
   }
-  // Admin voit uniquement les utilisateurs de son salon
   return await User.find({ salon: salonId }).select("-__v");
 };
 
@@ -16,7 +14,7 @@ exports.getUserById = async (id) => {
 };
 
 exports.updateUser = async (id, data) => {
-  const allowed = ["firstName", "lastName", "phone", "isActive", "role"];
+  const allowed = ["firstName", "lastName", "phone", "isActive", "role", "salon"];
   const updates = {};
   allowed.forEach(field => {
     if (data[field] !== undefined) updates[field] = data[field];
